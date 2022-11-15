6dfa4f5dee1aef34230d175ce1bc46a9c81e0302
Darcy Shen <66561265+da-tubi@users.noreply.github.com>
2022-11-15 19:38:44 +0800

    Forests of randomized trees (#11)

---
+++ rikai_sklearn/ensemble.py
@@ -0,0 +1,8 @@
+from rikai_sklearn.models import Regression, Classifier
+
+
+RandomForestRegressor = Regression
+ExtraTreesRegressor = Regression
+
+RandomForestClassifier = Classifier
+ExtraTreesClassifier = Classifier
