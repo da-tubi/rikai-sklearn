8721e172b5b06f0360a840326cff8f3e6563919d
Da Shen <da@tubi.tv>
2022-11-16 16:22:11 +0800

    clustering

--- rikai_sklearn/models/cluster.py
+++ rikai_sklearn/models/cluster.py
@@ -2,14 +2,14 @@ from typing import List
 from rikai_sklearn.models import SklearnModelType
 
 
-class Classifier(SklearnModelType):
+class Clustering(SklearnModelType):
     """Classification model type"""
 
     def schema(self) -> str:
         return "int"
 
-    def predict(self, x, *args, **kwargs) -> List[int]:
+    def predict(self, x, *args, **kwargs) -> List:
         assert self.model is not None
         return self.model.predict(x).tolist()
 
-MODEL_TYPE=Classifier()
+MODEL_TYPE=Clustering()
