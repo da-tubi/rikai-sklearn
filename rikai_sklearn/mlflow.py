c46f97eaa69d8c6af942cdd2b6008d5617cdda4d
Da Shen <da@tubi.tv>
2022-11-16 15:51:59 +0800

    cluster: get model type

--- rikai_sklearn/mlflow.py
+++ rikai_sklearn/mlflow.py
@@ -1,6 +1,6 @@
 import rikai
 from typing import Any, Optional
-from sklearn.base import RegressorMixin, ClassifierMixin, TransformerMixin
+from sklearn.base import RegressorMixin, ClassifierMixin, TransformerMixin, ClusterMixin
 
 
 def _get_model_type(model):
@@ -8,6 +8,8 @@ def _get_model_type(model):
         return "rikai_sklearn.models.regressor"
     elif isinstance(model, ClassifierMixin):
         return "rikai_sklearn.models.classifier"
+    elif isinstance(model, ClusterMixin):
+        return "rikai_sklearn.models.cluster"
     elif isinstance(model, TransformerMixin):
         return "rikai_sklearn.models.transformer"
     else:
